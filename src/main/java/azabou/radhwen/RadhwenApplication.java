037ac89de6772a587586122a4b6bfe3b0f3f2d0f
yosri azzabou <Yosriazzaboup9@gmail.com>
2024-01-19 14:57:38 +0100

    Initial commit

---
+++ src/main/java/azabou/radhwen/RadhwenApplication.java
@@ -0,0 +1,13 @@
+package azabou.radhwen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadhwenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadhwenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/azabou/radhwen/RadhwenApplicationTests.java
@@ -0,0 +1,13 @@
+package azabou.radhwen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadhwenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
